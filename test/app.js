f7bf0e6ff2bbb4873e7580a0798e5f63720051bf
avil13 <13.avil@gmail.com>
2016-04-17 14:36:45 +0300

    пример

--- test/app.js
+++ test/app.js
@@ -9,8 +9,45 @@ app.controller('TestCtrl', ['$scope', '$timeout', function($scope, $timeout) {
             '-e--[h1]': 'Hello world!'
         },
         '2': {
-            '-e--[p]':{
-                '-e--[span.xxx.yyy]': 'demo 2'
+            '-e--[p.well]': {
+                '-e--[span]': 'demo 2'
+            }
+        },
+        '3':{
+            '-e--[h3]':'if component',
+
+            '-e--[input.form-control]':{
+                'type':'text',
+                'ng-model': 'val'
+            },
+
+            '-if--[val=="1"]': {
+                '-e--[]':'show value {{val}}'
+            },
+
+            '-if--[val!="1"]': {
+                '-e--[]':'hide value {{val}}'
+            }
+        },
+        '4': {
+            '-e--[h3]':'switch component',
+
+            '-e--[select ng-model="selector".form-control]': {
+                '-e--[option value="1"]': 'show 1',
+                '-e--[option value="2"]': 'show 2',
+                '-e--[option value="3"]': 'show 3',
+            },
+            '-e--[.well]': '{{ selector }}',
+            '-switch--[selector]': {
+                '-case--[1]': {
+                    '-e--[p]': 'selected first item'
+                },
+                '-case--[2]': {
+                    '-e--[p]': 'selected second item'
+                },
+                '-case--[3]': {
+                    '-e--[p]': 'selected third item'
+                }
             }
         }
     };
